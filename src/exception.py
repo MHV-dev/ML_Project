1d9d46a66cfa9721b582948c101fd9572c5e9f9b
MHV-dev <harshamyneedi@gmail.com>
2025-10-21 14:47:54 +0530

    commit after prediction

--- src/exception.py
+++ src/exception.py
@@ -1,27 +1,30 @@
+# src/exception.py
+
 import sys
 import logging
 
-def error_msg_details(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error))
-    
-    return error_message
 
+def error_message_detail(error: Exception) -> str:
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    if exc_tb is not None and exc_tb.tb_frame is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        return f"Error occurred in script: {file_name} at line {line_number} | Error message: {str(error)}"
+    else:
+        return f"Error message: {str(error)}"
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message: Exception):
+        self.error_message = error_message_detail(error_message)
         super().__init__(error_message)
-        self.error_message = error_msg_details(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
 
-
+# Testing block
 if __name__ == "__main__":
     try:
         a = 1 / 0
     except Exception as e:
         logging.info("Divided by zero")
-        raise CustomException(e, sys)
+        raise CustomException(e)
